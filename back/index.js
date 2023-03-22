b9596989d99527c4dac39dd55ca92bf509624962
Adriane <adrianeprestes@hotmail.com>
2023-03-21 21:37:16 -0300

    connect banco

--- back/index.js
+++ back/index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000;
 const csv = require('node-csv').createParser();
+
+const mongodb = require('mongodb'); // incluiu essa linha
+
+const url_mongo = "mongodb+srv://adriprestes73:jr676c3ba@cluster0.9qxoixq.mongodb.net/?retryWrites=true&w=majority";
+
+const conexao = new mongodb.MongoClient(url_mongo);
+
 //CRUD
 //Create
 //Read
@@ -16,6 +23,13 @@ app.get("/entradas", function(req,res){
     csv.parseFile("estoque.csv", function(erro, valores){
         res.json(valores);
     })
+});
+
+app.get("/estoque", async function(req, res){
+    const estoque = conexao.db("sysexp").collection("estoque");
+    const resultado = await estoque.find({}).toArray();
+    //res.send("Oi");
+    res.json(resultado);
 });
 
 app.listen(port, () => {
