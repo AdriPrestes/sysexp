f61a7c9cf844813c811926bd467f10789a78197b
AdriPrestes <127901937+AdriPrestes@users.noreply.github.com>
2023-03-22 10:55:48 -0300

    Update index.js

--- back/index.js
+++ back/index.js
@@ -5,7 +5,7 @@ const csv = require('node-csv').createParser();
 
 const mongodb = require('mongodb'); // incluiu essa linha
 
-const url_mongo = "mongodb+srv://adriprestes73:jr676c3ba@cluster0.9qxoixq.mongodb.net/?retryWrites=true&w=majority";
+const url_mongo = "mongodb+srv://<user>:<pass>@cluster0.9qxoixq.mongodb.net/?retryWrites=true&w=majority";
 
 const conexao = new mongodb.MongoClient(url_mongo);
 
